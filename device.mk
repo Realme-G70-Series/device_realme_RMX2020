1e915ea953d6b67a0633e6c7fae18600d2d7ca14
Sarthak <sarthakroy2002@gmail.com>
2021-05-08 22:00:09 +0530

    RMX2020: Compile Realme Parts
    * https://github.com/HyperTeam/packages_apps_RealmeParts
    * But we will use your repo with changes

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ $(call inherit-product-if-exists, vendor/realme/RMX2020/RMX2020-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Parts
+$(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # VNDK
