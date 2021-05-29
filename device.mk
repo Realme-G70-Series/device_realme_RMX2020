dc42372aed256c04af255143ad3e1a36458b7614
techyminati <sinha.aryan03@gmail.com>
2021-05-29 15:08:45 +0530

    RMX2020: Drop Q IMS Jars
    * These are depreciated with VNDK 30
    
    Signed-off-by: Sarthak <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -69,11 +69,6 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
-# IMS
-PRODUCT_PACKAGES += \
-    mtk-ims \
-    mtk-ims-telephony
-
 # ImsInit hack
 PRODUCT_PACKAGES += \
     ImsInit
