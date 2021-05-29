b303e05fb35246ded7d3c79a603aa36624498fb7
Zinadin Zidan <zidan44@pixelexperience.org>
2021-05-29 18:21:28 +0530

    RMX2020: Import Stock Mediatek IMS jars from Stock 11
    * RMX2020_11_C.01

--- device.mk
+++ device.mk
@@ -73,6 +73,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ImsInit
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # Init
 PRODUCT_PACKAGES += \
     init.mt6768.rc \
