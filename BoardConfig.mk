cc70994439cc4c2ad7cbbeb82acf9d11ed0f1417
Sarthak <sarthakroy2002@gmail.com>
2021-05-30 00:00:52 +0530

    RMX2020: Remove TARGET_LD_SHIM_LIBS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,9 +141,6 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
-# Symbols
-TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
-
 # Treble
 BOARD_VNDK_VERSION := current
 
